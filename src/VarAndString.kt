af703f125c6a23c3b1169d0171051c57335e94ee
Smit <susmit025@gmail.com>
2022-11-15 18:29:29 +0530

    Variable and String Concat

---
+++ src/VarAndString.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    val a = "Joe"
+    val b = "Biden"
+    val no = 1
+    println("$a$b$no")
+
+    val longText = """
+             This is suppose
+             to be the text of
+             long text  
+             """.trimIndent()
+    println(longText)
+
+}
